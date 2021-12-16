a160da525aae693807cd8f8b1a605ed7953c353e
Артем <kysapka@gmail.com>
2021-12-17 00:44:56 +0300

    reqursion func fixed

--- try_catch_finally.js
+++ try_catch_finally.js
@@ -1,13 +1,30 @@
-function someFunc() {
-    try {
-        if(Math.random() <= 0.5) {
-            throw new Error('');
+// function someFunc() {
+//     try {
+//         if(Math.random() <= 0.5) {
+//             throw new Error('');
+//         }
+//         return 1;
+//     } catch (e) {
+//         return 2;
+//     } finally {
+//         return 3;
+//     }
+// }
+// console.log(someFunc());
+
+function  superSum(...args) {
+    let sum = 0;
+    let firstArgs = args[0];
+    function helper(firstArgs) {
+        if (!firstArgs) {
+            return sum
+        } else {
+            sum = sum + firstArgs
+            return helper
         }
-        return 1;
-    } catch (e) {
-        return 2;
-    } finally {
-        return 3;
     }
+    return helper(firstArgs)
 }
-console.log(someFunc());
+
+    const result = superSum(1)(2)(3)()
+    console.log(result)
