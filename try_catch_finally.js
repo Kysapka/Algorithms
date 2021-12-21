b75d4361d2e6b7f93a7a606ff76a615a0d425972
Артем <kysapka@gmail.com>
2021-12-22 02:22:36 +0300

    while and closers

--- try_catch_finally.js
+++ try_catch_finally.js
@@ -12,18 +12,47 @@
 // }
 // console.log(someFunc());
 
-function  superSum(...args) {
-    let sum = 0;
-    let firstArgs = args[0];
-    function helper(firstArgs) {
-        if (!firstArgs) {
-            return sum
-        } else {
-            sum = sum + firstArgs
-            return helper
-        }
-    }
-    return helper(firstArgs)
+// function  superSum(...args) {
+//     let sum = 0;
+//     let firstArgs = args[0];
+//     function helper(firstArgs) {
+//         if (!firstArgs) {
+//             return sum
+//         } else {
+//             sum = sum + firstArgs
+//             return helper
+//         }
+//     }
+//     return helper(firstArgs)
+// }
+//     const result = superSum(1)(2)(3)()
+//     console.log(result)
+
+// const arr = [1,2,3,4,5,6,7,8]
+//
+// console.log(arr.filter((el) => {
+//
+//     return el % 2;
+// }))
+// let arr = [1,2,[3,4,5,[6,7],8],9,10,[11,12,[13],[14, 15],16, [17, 18], 19, 20]];
+//
+// const myFlat = function(array) {
+//     let result = [];
+//     array.forEach((el) => {
+//         if(!Array.isArray(el)) {
+//             result.push(el)
+//         } else {
+//             result = [...result, ...myFlat(el)]
+//         }
+//     })
+//     return result;
+// }
+//
+// console.log(myFlat(arr));
+
+let i = 0;
+while (i++ < 4) {
+    setTimeout((j) => {
+        console.log(j)
+    }, i*1000, i)
 }
-    const result = superSum(1)(2)(3)()
-    console.log(result)
