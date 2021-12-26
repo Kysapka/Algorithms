f89d2492a811c00e0fa258364f99a13fb530c230
Артем <kysapka@gmail.com>
2021-12-26 15:18:42 +0300

    add custom bind and apply

--- .idea/customBind.js
+++ .idea/customBind.js
@@ -25,10 +25,31 @@
 
 
 
+Function.prototype.myApply = function (context, ...args) {
+    const savedThis = this;
+    return (function (...args2) {
+        const unicID = Date.now().toString()
+        console.log(unicID)
+        context[unicID] = savedThis
+        return context[unicID](...args, ...args2)
+        delete context[unicID]
+   })()
+}
 
 
+const obj1 = {
+    name: 'Vasya',
+    sayName: function () {
+        console.log('My name is ', this.name)
+    }
+}
 
+const obj2 = {
+    name: 'Ivan',
+}
 
+// obj1.sayName.bind(obj2)()
+obj1.sayName.myApply(obj2)
 
 
 
