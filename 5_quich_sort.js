db186c4ed97b771a43a8dcfbf8df47928d40f2ab
Артем <kysapka@gmail.com>
2022-01-28 00:02:17 +0300

    sort

--- 5_quich_sort.js
+++ 5_quich_sort.js
@@ -27,13 +27,34 @@ const arr = [0,3,2,5,6,8,23,9,4,2,1,2,9,6,4,1,7,-1, -5, 23,6,2,35,6,3,32,9,4,2,1
 // console.log('count', count)
 
 
-let c = 0
+// let c = 0
+//
+// let res = [...arr].sort((a,b) => {
+//     c++
+//     if (a > b) {
+//         return 1
+//     } else return -1
+// })
+//
+// console.log(res, c)
 
-let res = [...arr].sort((a,b) => {
-    c++
-    if (a > b) {
-        return 1
-    } else return -1
-})
-
-console.log(res, c)
+// const obj = {
+//     a: 3
+// }
+//
+// let a = 1
+//
+// const func1 = function () {
+//     console.log(this.a)
+// }
+//
+// const func2 = function(f) {
+//     let a = 2
+//     return f()
+// }
+//
+// func2(func1)
+//
+// obj.f = func1
+//
+// obj.f()
